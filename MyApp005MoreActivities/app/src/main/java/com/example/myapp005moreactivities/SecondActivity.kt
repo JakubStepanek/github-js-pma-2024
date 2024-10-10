bba3213315c5db11753c22f4f86083072492ea3d
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-10-10 15:14:17 +0200

    #5 second activity logic

--- MyApp005MoreActivities/app/src/main/java/com/example/myapp005moreactivities/SecondActivity.kt
+++ MyApp005MoreActivities/app/src/main/java/com/example/myapp005moreactivities/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapp005moreactivities
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +15,16 @@ class SecondActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_second)
 
-        
+        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+        // get data from Intent
+        val nickname = intent.getStringExtra("USER_NICKNAME")
+        tvInfo.text = "Data z první aktivity. Přezdívka: $nickname"
+
+        val btnClose = findViewById<Button>(R.id.btnClose)
+        btnClose.setOnClickListener {
+            finish()
+        }
+
+        val btnThirdActivity = findViewById<Button>(R.id.btnThirdSection)
     }
 }
