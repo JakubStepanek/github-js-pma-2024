b7ce1add609f075c822ccf87804da06650f457ec
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-10-17 11:59:54 +0200

    #6 Add Snackbar and Toast

--- MyApp006ToastSnackbar/app/src/main/java/com/example/myapp006toastsnackbar/MainActivity.kt
+++ MyApp006ToastSnackbar/app/src/main/java/com/example/myapp006toastsnackbar/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.myapp006toastsnackbar
 
+
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapp006toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //button actions
+        binding.btnShowToast.setOnClickListener {
+            showToast()
         }
+
+        binding.btnShowSnackbar.setOnClickListener {
+            showSnackbar()
+        }
+    }
+
+    //
+    private fun showSnackbar() {
+        Snackbar.make(binding.root, "Když je to pravda, tak je to pravda.", 7000)
+            .setBackgroundTint(Color.GREEN)
+            .setTextColor(Color.BLACK)
+            .setAction("Zavřít") {
+                Toast.makeText(this, "Zavírám Snackbar", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .show()
+    }
+
+    private fun showToast() {
+        val toast = Toast.makeText(
+            this,
+            "Směr je tímto směrem, protože jinak by realita neodpovídala skutečnosti.",
+            Toast.LENGTH_LONG
+        )
+
+        .show()
+
     }
 }
