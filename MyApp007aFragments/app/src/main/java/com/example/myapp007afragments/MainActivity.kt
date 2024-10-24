ecc12face9addd059e3d76cbe712f3f54a5f1ebb
Jakub Štěpánek <kubin@Jakubs-MacBook-Air.local>
2024-10-24 11:08:42 +0200

    #7 add starter pack

---
+++ MyApp007aFragments/app/src/main/java/com/example/myapp007afragments/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapp007afragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp007afragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    //setContentView(R.layout.activity_main)
+
+
+    }
+}
