9932d754c1b1c9681a6a40980b9e1c59f9e6f40d
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-11-05 09:46:50 +0100

    #9 project refresh

--- MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
+++ MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -24,6 +25,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    MyComposable()
                 }
             }
         }
@@ -43,5 +45,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     MyApp009aDataStoreTheme {
         Greeting("Android")
+    }
+}
+@Composable
+fun MyComposable(){
+    Column {
+        Text(text = "Hello")
+        Text(text = "World")
     }
 }
