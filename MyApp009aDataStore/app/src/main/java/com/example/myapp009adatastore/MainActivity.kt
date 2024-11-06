fca5f3165703e44213228a7f6a68ac89908f996b
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-11-06 23:34:13 +0100

    #9 add form elements

--- MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
+++ MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
@@ -1,18 +1,31 @@
+@file:Suppress("unused", "UNUSED_VARIABLE", "PreviewMustBeTopLevelFunction")
+
 package com.example.myapp009adatastore
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapp009adatastore.ui.theme.MyApp009aDataStoreTheme
+//důležitý import!!!
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,37 +33,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApp009aDataStoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    MyComposable()
-                }
+                UserForm()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UserForm() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        var text by remember { mutableStateOf("") }
+
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { newText -> text = newText },
+            label = { Text("Jméno") }
+        )
+
+
+        val pattern = remember { Regex("^\\d+\$") }
+        var age by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = age,
+            label = { Text("Věk") },
+            onValueChange = {
+                if (it.isEmpty() || it.matches(pattern)) {
+                    age = it
+                }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+
+        //todo: Check box
+
+        var checked by remember { mutableStateOf(true) }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                "Minimal checkbox"
+            )
+            Checkbox(
+                checked = checked,
+                onCheckedChange = { checked = it }
+            )
+        }
+
+        Text(
+            if (checked) "Checkbox is checked" else "Checkbox is unchecked"
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UserFormPreview() {
     MyApp009aDataStoreTheme {
-        Greeting("Android")
-    }
-}
-@Composable
-fun MyComposable(){
-    Column {
-        Text(text = "Hello")
-        Text(text = "World")
+        UserForm()
     }
 }
