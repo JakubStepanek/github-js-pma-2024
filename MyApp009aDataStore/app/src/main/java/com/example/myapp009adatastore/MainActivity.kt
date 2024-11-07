5ab8e9850454390d8374b07989b228405498d7fc
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-11-07 09:29:01 +0100

    #9 Form ready

--- MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
+++ MyApp009aDataStore/app/src/main/java/com/example/myapp009adatastore/MainActivity.kt
@@ -2,18 +2,23 @@
 
 package com.example.myapp009adatastore
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,6 +30,7 @@ import com.example.myapp009adatastore.ui.theme.MyApp009aDataStoreTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -78,18 +84,25 @@ fun UserForm() {
         Row(
             verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text(
-                "Minimal checkbox"
-            )
+
             Checkbox(
-                checked = checked,
+                checked = !checked,
                 onCheckedChange = { checked = it }
             )
+            Text(
+                "Potvrzení, že jsem starší 18 let."
+            )
         }
-
-        Text(
-            if (checked) "Checkbox is checked" else "Checkbox is unchecked"
-        )
+        Row() {
+            Button(
+                modifier = Modifier.padding(0.dp, 0.dp, 20.dp, 0.dp),
+                onClick = { saveData() }) {
+                Text("Uložit")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text("Načíst")
+            }
+        }
     }
 }
 
@@ -100,4 +113,8 @@ fun UserFormPreview() {
     MyApp009aDataStoreTheme {
         UserForm()
     }
+}
+
+fun saveData(){
+
 }
