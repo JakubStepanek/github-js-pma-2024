5bbbd792b18a28950cf70c5a30a68119be16f324
Jakub Štěpánek <jakub.stepanek@tul.cz>
2024-12-12 11:13:01 +0100

    #14 Firebase permission denied

--- MyApp014aSharedTaskList/app/src/main/java/com/example/myapp014asharedtasklist/MainActivity.kt
+++ MyApp014aSharedTaskList/app/src/main/java/com/example/myapp014asharedtasklist/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.text.InputType
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapp014asharedtasklist.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
